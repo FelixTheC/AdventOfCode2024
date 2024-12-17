73105997eca538f390ea5b911512822e875933c5
felix <felixeisenmenger@gmx.net>
2024-12-17 18:56:46 +0100

    Refactor grid search logic and add modular unit tests
    Separated grid search functionality into distinct helper functions for better readability and maintainability. Introduced comprehensive unit tests for each search direction to verify correctness. Updated input file handling to use "puzzle_input.txt" instead of "test_input.txt."

--- Day_4/src/lib.rs
+++ Day_4/src/lib.rs
@@ -1,6 +1,7 @@
 
 pub fn read_test_input() -> Vec<Vec<char>> {
-    let input = include_str!("../test_input.txt");
+    // let input = include_str!("../test_input.txt");
+    let input = include_str!("../puzzle_input.txt");
 
     let mut result: Vec<Vec<char>> = Vec::new();
 
@@ -57,6 +58,12 @@ impl Xmas {
         self.x && self.m && self.a && self.s
     }
 
+    fn complete_result(&self) -> u16 {
+        if self.x && self.m && self.a && self.s { 1 }
+        else { 0 }
+    }
+
+    #[allow(dead_code)]
     fn reset(&mut self) {
         self.x = false;
         self.m = false;
@@ -65,129 +72,136 @@ impl Xmas {
     }
 }
 
-fn ceres_search_sub(grid: &Vec<Vec<char>>, row: usize, col: usize) -> u16 {
-    let mut result: u16 = 0;
+fn find_to_the_right(grid: &Vec<Vec<char>>, row: usize, col: usize) -> u16 {
     let mut xmas = Xmas::new();
-    xmas.set_token('X');
-
-    // look horizontal to the right side
-    let max_col = grid[row].len() - 4;
-    if col <= max_col {
+    if col <= grid[row].len() - 4 {
         for idx in 0 .. 4 {
             if grid[row][col + idx] == xmas.next_token() {
                 xmas.set_token(grid[row][col + idx]);
+            } else {
+                break;
             }
         }
     }
+    xmas.complete_result()
+}
 
-    if xmas.is_complete() {
-        result += 1;
-    }
-
-    xmas.reset();
-
-    if col >= 4 {
+fn find_to_the_left(grid: &Vec<Vec<char>>, row: usize, col: usize) -> u16 {
+    let mut xmas = Xmas::new();
+    if col >= 3 {
         // look horizontal to the left side
         for idx in 0 .. 4 {
             if grid[row][col - idx] == xmas.next_token() {
                 xmas.set_token(grid[row][col - idx]);
+            } else {
+                break;
             }
         }
     }
+    xmas.complete_result()
+}
 
-    if xmas.is_complete() {
-        result += 1;
-    }
-
-    xmas.reset();
-
+fn find_top_to_bottom(grid: &Vec<Vec<char>>, row: usize, col: usize) -> u16 {
+    let mut xmas = Xmas::new();
     // look vertical down
     if row <= grid.len() - 4 {
         for idx in 0 .. 4 {
             if grid[row + idx][col] == xmas.next_token() {
                 xmas.set_token(grid[row + idx][col]);
+            } else {
+                break;
             }
         }
     }
+    xmas.complete_result()
+}
 
-    if xmas.is_complete() {
-        result += 1;
-    }
-
-    xmas.reset();
-
+fn find_bottom_to_top(grid: &Vec<Vec<char>>, row: usize, col: usize) -> u16 {
+    let mut xmas = Xmas::new();
     // look vertical up
-    if row >= 4 {
+    if row >= 3 {
         for idx in 0 .. 4 {
             if grid[row - idx][col ] == xmas.next_token() {
                 xmas.set_token(grid[row - idx][col]);
+            } else {
+                break;
             }
         }
     }
+    xmas.complete_result()
+}
 
-    if xmas.is_complete() {
-        result += 1;
-    }
-
-    xmas.reset();
-
+fn find_upper_left_to_bottom_right(grid: &Vec<Vec<char>>, row: usize, col: usize) -> u16 {
+    let mut xmas = Xmas::new();
     // diagonal lookup
     for idx in 0 .. 4 {
         if row + idx < grid.len() && col + idx < grid[row].len() {
             if grid[row + idx][col + idx] == xmas.next_token() {
                 xmas.set_token(grid[row + idx][col + idx]);
+            } else {
+                break;
             }
         }
     }
+    xmas.complete_result()
+}
 
-    if xmas.is_complete() {
-        result += 1;
-    }
-
-    xmas.reset();
-
+fn find_upper_left_to_bottom_left(grid: &Vec<Vec<char>>, row: usize, col: usize) -> u16 {
+    let mut xmas = Xmas::new();
     // diagonal lookup
     for idx in 0 .. 4 {
-        if row + idx < grid.len() && idx <= col && col - idx > 0 {
+        if row + idx < grid.len() && idx <= col {
             if grid[row + idx][col - idx] == xmas.next_token() {
                 xmas.set_token(grid[row + idx][col - idx]);
+            } else {
+                break;
             }
         }
     }
+    xmas.complete_result()
+}
 
-    if xmas.is_complete() {
-        result += 1;
-    }
-
-    xmas.reset();
-
+fn find_bottom_left_to_upper_left(grid: &Vec<Vec<char>>, row: usize, col: usize) -> u16 {
+    let mut xmas = Xmas::new();
     // diagonal lookup
     for idx in 0 .. 4 {
-        if idx <= row && idx <= col && row >= 4 && col >= 4 {
+        if idx <= row && idx <= col && row >= 3 && col >= 3 {
             if grid[row - idx][col - idx] == xmas.next_token() {
                 xmas.set_token(grid[row - idx][col - idx]);
+            } else {
+                break;
             }
         }
     }
+    xmas.complete_result()
+}
 
-    if xmas.is_complete() {
-        result += 1;
-    }
-
-    xmas.reset();
-
+fn find_bottom_left_to_upper_right(grid: &Vec<Vec<char>>, row: usize, col: usize) -> u16 {
+    let mut xmas = Xmas::new();
     // diagonal lookup
     for idx in 0 .. 4 {
-        if idx <= row && idx <= col && row >= 4 && col <= grid[row].len() - 4 {
+        if idx <= row && col <= grid[row].len() - 4 {
             if grid[row - idx][col + idx] == xmas.next_token() {
                 xmas.set_token(grid[row - idx][col + idx]);
+            } else {
+                break;
             }
         }
     }
+    xmas.complete_result()
+}
 
-    if xmas.is_complete() {
-        result += 1;
-    }
+fn ceres_search_sub(grid: &Vec<Vec<char>>, row: usize, col: usize) -> u16 {
+    let mut result: u16 = 0;
+
+    result += find_to_the_right(grid, row, col);
+    result += find_to_the_left(grid, row, col);
+    result += find_top_to_bottom(grid, row, col);
+    result += find_bottom_to_top(grid, row, col);
+    result += find_upper_left_to_bottom_right(grid, row, col);
+    result += find_upper_left_to_bottom_left(grid, row, col);
+    result += find_bottom_left_to_upper_left(grid, row, col);
+    result += find_bottom_left_to_upper_right(grid, row, col);
 
     result
 }
@@ -205,4 +219,190 @@ pub fn ceres_search(grid: &Vec<Vec<char>>) -> u16 {
     }
 
     result
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_read_from_left_to_right() {
+        {
+            let input = vec![vec!['M','M','M','S','X','X','M','A','S','M']];
+
+            assert_eq!(find_to_the_right(&input, 0, 4), 0);
+            assert_eq!(find_to_the_right(&input, 0, 5), 1);
+        }
+        {
+            let input = vec![vec!['M','M','M','S','X','M','X','M','A','S']];
+            assert_eq!(find_to_the_right(&input, 0, 6), 1);
+        }
+        {
+            let input = vec![vec!['X','M','A','S','M','M','M','S','X','M']];
+            assert_eq!(find_to_the_right(&input, 0, 0), 1);
+        }
+    }
+
+    #[test]
+    fn test_read_from_right_to_left() {
+        {
+            let input = vec![vec!['M','M','M','S','X','X','M','A','S','M']];
+            assert_eq!(find_to_the_left(&input, 0, 4), 0);
+            assert_eq!(find_to_the_left(&input, 0, 5), 0);
+        }
+        {
+            let input = vec![vec!['S','A','M','X','M','M','S','A','M','X']];
+            assert_eq!(find_to_the_left(&input, 0, 3), 1);
+            assert_eq!(find_to_the_left(&input, 0, 9), 1);
+        }
+        {
+            let input = vec![vec!['M','M','S','S','A','M','X','A','M','X']];
+            assert_eq!(find_to_the_left(&input, 0, 6), 1);
+            assert_eq!(find_to_the_left(&input, 0, 9), 0);
+        }
+    }
+
+    #[test]
+    fn test_read_from_top_to_bottom() {
+        {
+            let input = vec![
+                vec!['M','X','S'],
+                vec!['X','M','A'],
+                vec!['S','A','M'],
+                vec!['A','S','S']
+            ];
+
+            assert_eq!(find_top_to_bottom(&input, 0, 0), 0);
+            assert_eq!(find_top_to_bottom(&input, 0, 1), 1);
+            assert_eq!(find_top_to_bottom(&input, 0, 2), 0);
+        }
+        {
+            let input = vec![
+                vec!['A','S','S'],
+                vec!['X','M','A'],
+                vec!['M','X','S'],
+                vec!['X','M','A'],
+                vec!['S','A','M'],
+                vec!['A','S','S'],
+                vec!['M','X','S']
+            ];
+
+            assert_eq!(find_top_to_bottom(&input, 0, 1), 0);
+            assert_eq!(find_top_to_bottom(&input, 1, 1), 0);
+            assert_eq!(find_top_to_bottom(&input, 2, 1), 1);
+        }
+    }
+
+    #[test]
+    fn test_read_from_bottom_to_top() {
+        {
+            let input = vec![
+                vec!['M','S','S'],
+                vec!['X','A','A'],
+                vec!['S','M','M'],
+                vec!['A','X','S']
+            ];
+
+            assert_eq!(find_bottom_to_top(&input, 3, 0), 0);
+            assert_eq!(find_bottom_to_top(&input, 3, 1), 1);
+            assert_eq!(find_bottom_to_top(&input, 3, 2), 0);
+        }
+
+        {
+            let input = vec![
+                vec!['A','S','S'],
+                vec!['X','A','A'],
+                vec!['M','M','S'],
+                vec!['X','X','A'],
+                vec!['S','A','M'],
+                vec!['A','S','S'],
+                vec!['M','X','S']
+            ];
+
+            assert_eq!(find_bottom_to_top(&input, 3, 0), 0);
+            assert_eq!(find_bottom_to_top(&input, 6, 1), 0);
+            assert_eq!(find_bottom_to_top(&input, 3, 1), 1);
+        }
+    }
+
+    #[test]
+    fn test_find_upper_left_to_bottom_right()
+    {
+        {
+            let input = vec![
+                vec!['X','S','S','M'],
+                vec!['X','M','A','S'],
+                vec!['S','M','A','X'],
+                vec!['A','X','S','S']
+            ];
+
+            assert_eq!(find_upper_left_to_bottom_right(&input, 0, 0), 1);
+            assert_eq!(find_upper_left_to_bottom_right(&input, 1, 0), 0);
+        }
+        {
+            let input = vec![
+                vec!['X','S','S','M','X','S','S','M'],
+                vec!['X','M','A','S','X','M','A','S'],
+                vec!['S','M','A','X','S','M','A','X'],
+                vec!['X','S','S','X','X','S','S','M'], //<--
+                vec!['X','M','A','S','M','M','A','S'],
+                vec!['S','M','A','X','S','A','A','X'],
+                vec!['A','X','S','S','A','X','S','S']
+            ];
+
+            assert_eq!(find_upper_left_to_bottom_right(&input, 3, 3), 1);
+            assert_eq!(find_upper_left_to_bottom_right(&input, 0, 0), 0);
+        }
+    }
+
+    #[test]
+    fn test_find_upper_left_to_bottom_left()
+    {
+        let input = vec![
+            vec!['X','S','S','M','X','S','S','X'],
+            vec!['X','M','A','S','X','M','M','S'],
+            vec!['S','M','A','X','S','A','A','X'],
+            vec!['X','A','M','X','S','S','S','M'],
+            vec!['X','A','M','S','M','A','A','S'],
+            vec!['S','M','A','X','S','A','M','X'], // <--
+            vec!['A','X','S','S','A','X','S','X']
+        ];
+
+        assert_eq!(find_upper_left_to_bottom_left(&input, 2, 3), 1);
+        assert_eq!(find_upper_left_to_bottom_left(&input, 0, 7), 1);
+    }
+
+    #[test]
+    fn test_find_bottom_left_to_upper_left()
+    {
+        let input = vec![
+            vec!['X','S','S','M','X','S','S','S'],
+            vec!['S','M','A','S','X','M','A','S'],
+            vec!['S','A','A','X','S','M','A','X'],
+            vec!['X','A','M','S','S','S','S','M'],
+            vec!['X','A','A','X','M','A','A','S'],
+            vec!['S','M','A','X','S','A','M','X'], // <--
+            vec!['X','X','S','S','A','X','S','X']
+        ];
+
+        assert_eq!(find_bottom_left_to_upper_left(&input, 6, 7), 1);
+        assert_eq!(find_bottom_left_to_upper_left(&input, 4, 3), 1);
+    }
+
+    #[test]
+    fn test_find_bottom_left_to_upper_right()
+    {
+        let input = vec![
+            vec!['X','S','S','M','X','S','S','S'],
+            vec!['S','M','A','S','X','M','A','S'],
+            vec!['S','A','A','X','S','M','A','X'],
+            vec!['X','A','M','S','X','S','S','M'],
+            vec!['X','A','A','X','M','A','A','S'],
+            vec!['S','M','A','X','S','A','M','X'], // <--
+            vec!['X','X','S','S','A','X','S','X']
+        ];
+
+        assert_eq!(find_bottom_left_to_upper_right(&input, 6, 0), 1);
+        assert_eq!(find_bottom_left_to_upper_right(&input, 3, 4), 1);
+    }
 }
